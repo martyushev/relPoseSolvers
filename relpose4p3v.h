ce2ff83e62169f543ac158768bcb32e1453e4507
martyushev <72862703+martyushev@users.noreply.github.com>
2022-06-02 21:29:35 +0500

    Update relpose4p3v.h

--- relpose4p3v.h
+++ relpose4p3v.h
@@ -1,17 +1,3 @@
-/***********************************************************************************************************
-This is an implementation of the 4-point 3-view algorithm for metric reconstruction from
-
-Nister, D., Schaffalitzky, F.: Four Points in Two or Three Calibrated Views: Theory and Practice.
-Int. J. of Computer Vision 67(2), 211-231 (2006).
-
-The program is free for academic/research purposes. It is distributed in the hope that it can be useful but with no warranty.
-
-Evgeniy Martyushev, 2015
-
-************************************************************************************************************/
-
-
-
 #include "auxArrays.h"
 #include "p4p.h"
 
